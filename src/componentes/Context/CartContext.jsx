a1ea5f6cac369ca0748bbd79dc2ea8c49ad62549
santiago_viale <santicbsn9@gmail.com>
2022-11-24 18:27:16 -0300

    hola

--- src/componentes/Context/CartContext.jsx
+++ src/componentes/Context/CartContext.jsx
@@ -5,7 +5,10 @@ export const cartContext = createContext()
 export function CartContextProvider({children}){
   const [cart, setCart] = useState([])
   function addToCart(product,count){
-
+    let newCart = cart.map(item => item)
+    product.count = count
+      newCart.push(product)
+      setCart(newCart)
   }
   return (<cartContext.Provider value={{ cart, addToCart}}>{children}</cartContext.Provider>)
 }
